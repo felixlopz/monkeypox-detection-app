d8954b9c2c93e49ee830852baa5ccf82bb67058e
Felix Lopez <felixlopzd@gmail.com>
2023-06-20 13:56:28 -0400

    removing comments from metro.config.js

--- mda/metro.config.js
+++ mda/metro.config.js
@@ -1,20 +1,9 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const {getDefaultConfig} = require('expo/metro-config')
 
-// module.exports = getDefaultConfig(__dirname);
-
 const defaultConfig = getDefaultConfig(__dirname)
 const {assetExts} = defaultConfig.resolver
 const config = defaultConfig
 config.resolver.assetExts = [...assetExts, 'bin']
 
 module.exports = config
-
-// module.exports = (async () => {
-//   return {
-//     resolver: {
-//       // Add bin to assetExts
-//       assetExts: [...assetExts, 'bin']
-//     }
-//   }
-// })()
